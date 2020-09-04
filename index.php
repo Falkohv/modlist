4bf690eb351b7cc250f132762fb073002eb57d70
Falkohv <tnkfalcon@gmail.com>
2020-09-04 12:46:12 -0300

    correçao dos nomes

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
     $style = 'home';
-    include('header.php');
+    include('header-home.php');
     include('includes/pages/home.php');
     include('footer.php');
 ?>
--- ultra.php
+++ ultra.php
@@ -1,6 +1,6 @@
 <?php
     $style = 'ultra';
-    include('header-ultra.php');
+    include('header.php');
     include('includes/pages/ultra.php');
     include('footer.php');
 ?>
