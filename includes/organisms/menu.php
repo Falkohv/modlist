e002a52d2775d419c5907c8153028f1f5068650c
Falkohv <tnkfalcon@gmail.com>
2020-09-04 12:48:09 -0300

    recuperação da pasta organisms

---
+++ includes/organisms/menu.php
@@ -0,0 +1,8 @@
+<nav class="wow fadeIn" data-wow-duration="1.5s" data-wow-delay=".2s">
+    <ul>
+        <li><a href="index.php">INÍCIO</a></li>
+        <li><a href="#">LISTAS</a></li>
+        <li><a href="#">SHOWCASES</a></li>
+        <li><a href="https://www.youtube.com/channel/UCQXI1SWuUaom_cYv3_qCb_w">CANAL</a></li>
+    </ul>                            
+</nav> 
