571d2e8006969699cfb28a11abecfa3f425e8b4f
Falkohv <tnkfalcon@gmail.com>
2020-09-04 20:25:55 -0300

    ta foda

--- includes/organisms/menu.php
+++ includes/organisms/menu.php
@@ -1,16 +1,8 @@
 <nav class="wow fadeIn" data-wow-duration="1.5s" data-wow-delay=".2s">
     <ul>
         <li><a href="index.php">INÍCIO</a></li>
-<<<<<<< HEAD
-        <li><a href="#">LISTAS</a></li>
-        <li><a href="#">SHOWCASES</a></li>
-        <li><a href="https://www.youtube.com/channel/UCQXI1SWuUaom_cYv3_qCb_w">CANAL</a></li>
-    </ul>                            
-</nav> 
-=======
         <li><a href="ultra.php">ULTRA</a></li>
         <li><a href="showcases.php">SHOWCASES</a></li>
         <li><a href="https://www.youtube.com/channel/UCQXI1SWuUaom_cYv3_qCb_w">CANAL</a></li>
     </ul>                            
 </nav> 
->>>>>>> php
