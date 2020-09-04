46b3386cbfe0ba07af441f80820180bcf8897764
Falkohv <tnkfalcon@gmail.com>
2020-09-04 20:02:42 -0300

    ajustes finais

--- includes/organisms/menu.php
+++ includes/organisms/menu.php
@@ -1,7 +1,7 @@
 <nav class="wow fadeIn" data-wow-duration="1.5s" data-wow-delay=".2s">
     <ul>
         <li><a href="index.php">INÍCIO</a></li>
-        <li><a href="#">LISTAS</a></li>
+        <li><a href="ultra.php">ULTRA</a></li>
         <li><a href="showcases.php">SHOWCASES</a></li>
         <li><a href="https://www.youtube.com/channel/UCQXI1SWuUaom_cYv3_qCb_w">CANAL</a></li>
     </ul>                            
