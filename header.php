f61d6679d9771dfb699d787e92526959f6d261bc
Falkohv <tnkfalcon@gmail.com>
2020-09-08 15:39:04 -0300

    configurações wordpress

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Falkohv</title>
+        <title><?php bloginfo('name'); echo " | "; bloginfo('description'); ?></title>
         <link rel="stylesheet" href="css/<?php echo $style; ?>.css" />
         <link rel="stylesheet" href="css/geral.css" />
         <link rel="stylesheet" href="css/plugins.css" />
